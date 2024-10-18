114bcb5c1429e6785e97fc82edea3023a23eeeee
marcelosperez <jhkk1408@gmail.com>
2024-10-18 14:43:50 -0300

    Updated files

---
+++ Partie 1/variables.java
@@ -0,0 +1,32 @@
+
+ /* 
+
+Déclarez des variables
+======================
+  
+Nous y voilà, vous allez enfin découvrir comment programmer une voiture autonome, à moins que vous ne vouliez concevoir 
+un robot pour faire la cuisine ?
+
+Bon, il va tout de même falloir commencer par les bases. Cela tombe bien, nous y sommes. Et nous allons tout de suite 
+voir les notions indispensables ! Commençons par les variables !
+
+Qu’est-ce qu’une variable ?
+
+Une variable est un outil contenant une donnée, par exemple un mot ou un chiffre, et qui va être utilisée par un programme.
+
+En quoi est-ce si important ?
+
+Eh bien, concrètement, un programme est constamment en train de manipuler des variables, certaines qu’on lui donne, d’autres 
+qu’il crée de lui-même.
+
+On peut comparer une variable à une boîte dans laquelle est rangée une information. Cette boîte est stockée sur une étagère 
+dans un entrepôt gigantesque. L'emplacement de chaque boîte de cet entrepôt est soigneusement enregistré, et géré par l’ordinateur.
+
+ATTENTION: Une donnée placée dans une variable s'appelle une valeur, et chaque boîte contient une valeur.
+
+Pour savoir à quoi sert chaque boîte, vous devez l’étiqueter. Avec la programmation, c'est la même chose : 
+vous attribuez un nom à votre variable.
+
+*/
+
+
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# FRANCES
+# Learn Java in French
  
- Versão de textos, gramática, vocabulário
+Apprenez à coder en Java sur OpenClassrooms
+
+Ce cours suit une progression logique que l’on a séquencée en 3 parties. Chaque partie contient plusieurs chapitres, qu’il est préférable de suivre dans l’ordre.
+
+Dans ces chapitres, vous retrouverez régulièrement :
+
+-- des screencasts tutoriels. Ce sont des vidéos de démonstration qui permettent de suivre étape par étape la réalisation d’un point du cours directement sur l’ordinateur de l’expert-formateur ;
+
+-- des exercices dans les sections "À vous de jouer". C’est l’occasion de mettre en pratique ; ils sont précieux pour accélérer votre apprentissage !
+
+Dans ce cours, vous mettrez ce que vous avez appris en pratique dans des exercices de code interactifs sur Replit. 
